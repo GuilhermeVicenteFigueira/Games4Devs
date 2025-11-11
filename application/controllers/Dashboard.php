41787b64774a21b6423238c3900c9c41b475274a
guilherme.vicente <guilherme.vicente@fabricadecodigos.com.br>
2025-11-11 12:48:59 -0300

    Feat: Pagina Principal e Pagina de Games

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,11 +1,19 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller 
+class Dashboard extends CI_Controller
 {
-    public function index ()
+    public function index()
     {
+        $this->load->model("Games_model");
+        $data['games'] = $this->Games_model->index();
         $data['title'] =  "Dashboard - CodeIgniter";
-        $this->load->view('pages/dashboard', $data);
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/nav_top', $data);
+        $this->load->view('pages/Dashboard_view', $data);
+        $this->load->view('templates/footer', $data);
+        $this->load->view('templates/js', $data);
+        
     }
 }
---
+++ application/controllers/Games.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Games extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->model('Games_model');
+        $data['games'] = $this->Games_model->index();
+        $data['title'] = "Games - CodeIgniter";
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/nav_top', $data);
+        $this->load->view('pages/Games_view', $data);
+        $this->load->view('templates/footer', $data);
+        $this->load->view('templates/js', $data);
+    }
+
+    public function new()
+    {
+        $data['title'] = "Games - CodeIgniter";
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/nav_top', $data);
+        $this->load->view('pages/From-Games_view', $data);
+        $this->load->view('templates/footer', $data);
+        $this->load->view('templates/js', $data);
+    }
+}
